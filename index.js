c99d6aec3a1af2f6d603f9adb62aeeb292d39a67
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-27 14:29:41 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
