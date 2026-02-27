b00316dfc8a66daa9f35b7ed265b4b8a9210cbe0
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-27 14:51:19 +0200

    Update lock.js

--- commands/prefix/lock.js
+++ commands/prefix/lock.js
@@ -4,6 +4,7 @@ module.exports = {
   name: "lock",
   adminOnly: true,
   permissions: PermissionFlagsBits.ManageChannels,
+  cooldown: 10, // 👈 هنا
 
   async execute(message, args, client, sendTemp) {
     await message.channel.permissionOverwrites.edit(
