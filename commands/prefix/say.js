7ce333d18af618bb70b0803a632c337f9f84f55e
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-27 14:51:45 +0200

    Create say.js

---
+++ commands/prefix/say.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "say",
+  adminOnly: true,
+  permissions: PermissionFlagsBits.Administrator,
+
+  async execute(message, args, client, sendTemp) {
+    const text = args.join(" ");
+    if (!text) return sendTemp("❌ اكتب كلام.");
+
+    message.channel.send(text);
+  }
+};
