3dd093d1e7ef11296cf44fed4d416b8daaf9ceeb
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-27 14:59:57 +0200

    Create show.js

---
+++ commands/prefix/show.js
@@ -0,0 +1,17 @@
+const { PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+  name: "show",
+  adminOnly: true,
+  permissions: PermissionFlagsBits.ManageChannels,
+  cooldown: 10,
+
+  async execute(message, args, client, sendTemp) {
+    await message.channel.permissionOverwrites.edit(
+      message.guild.roles.everyone,
+      { ViewChannel: true }
+    );
+
+    sendTemp("👀 تم إظهار الروم.");
+  }
+};
